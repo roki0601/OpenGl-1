d43e064c43f296db2594d281a1e8497c16d3fbb1
Шамиль <shamilsatlykovvv@gmail.com>
2023-02-11 19:19:57 +0500

    Tutorial 2

---
+++ 1 lab/Header.h
@@ -0,0 +1,18 @@
+#pragma once
+struct Vector3f
+{
+    float x;
+    float y;
+    float z;
+
+    Vector3f()
+    {
+    }
+
+    Vector3f(float _x, float _y, float _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+};
